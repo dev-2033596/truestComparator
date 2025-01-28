b398e98a8225cb8dce75f54525a09f237bcfc890
dev-2033596 <2033596@etu.cchic.ca>
2025-01-27 22:45:52 -0500

    The whole code in one commit cause i'm just too good

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrueComparator comparator = new TrueComparator();
+            TrueComparator comparator1 = new TrueComparator();
+            int cpt = 0;
+            while (!false.Equals(comparator.CompareTo(comparator1) == 1 ? !false : false) == true)
+            {
+                cpt++;
+                Console.WriteLine(cpt + ". Je suis un beau garcon");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/TrueComparator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class TrueComparator : IComparable<TrueComparator>
+    {
+        public int CompareTo(TrueComparator? other)
+        {
+            if (true.Equals((bool)!true.Equals(false) == (bool)false.Equals(false)))
+            {
+                return 1; 
+            }
+            return 0;
+        }
+    }
+}
